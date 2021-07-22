d40150ec6da26733ceb415aa7d2b7701ad946986
Pancharatna Doley <pancharatna.doley@aexp.com>
2021-07-22 21:06:22 +0530

    Empty Project

---
+++ src/main/java/com/pdoley/eserver/EurekaServerAppApplication.java
@@ -0,0 +1,13 @@
+package com.pdoley.eserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerAppApplication.class, args);
+	}
+
+}
